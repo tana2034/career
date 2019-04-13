f639a6d1840fc149f46d7291b17bfa77c1e69f73
tana2034 <2034tana@gmail.com>
2019-04-13 16:49:19 +0900

    #31 switch to the vuex module mode

---
+++ store/headers.js
@@ -0,0 +1,21 @@
+export const state = () => ({
+  list: [
+    {
+      text: 'term',
+      align: 'left',
+      sortable: false,
+      value: 'name'
+    },
+    {
+      text: 'title',
+      align: 'left',
+      value: 'title'
+    },
+    {
+      text: 'description',
+      align: 'left',
+      sortable: false,
+      value: 'description'
+    }
+  ]
+})
