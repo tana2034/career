397ee8b3246d061d7f80a033337c0d56863a3110
tana2034 <2034tana@gmail.com>
2019-04-18 14:19:50 +0900

    #37 delete table and add card

--- components/WorkExperienceTable.vue
+++ components/WorkExperienceTable.vue
@@ -1,25 +1,33 @@
 <template>
-  <v-flex xs12 lg12 mb-3>
-    <p class="title">
-      {{ work.company }}
-    </p>
-    <v-data-table
-      :headers="headers"
-      :items="work.contents"
-      class="elevation-1"
-      hide-actions
-    >
-      <template v-slot:items="props">
-        <td>{{ props.item.from }} - {{ props.item.to }}</td>
-        <td class="text-xs-right">
-          {{ props.item.title }}
-        </td>
-        <td class="text-xs-right">
-          {{ props.item.description }}
-        </td>
-      </template>
-    </v-data-table>
-  </v-flex>
+  <v-layout row wrap>
+    <v-flex xs12 md12 xl12 mb-1 mt-3>
+      <v-subheader light>
+        <div><span>{{ work.from }}-{{ work.to }}</span><h4>{{ work.company }}</h4>
+        </div>
+      </v-subheader>
+    </v-flex> 
+    <v-flex xs12 md12 xl12>
+      <v-card
+        v-for="(content, i) in work.contents" 
+        :key="i"
+        :content="content"
+      >
+        <v-card-title primary-title>
+          <div>
+            <h4>{{ content.title }}</h4>
+            <div>
+              {{ content.from }} - {{ content.to }}
+            </div>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <div>
+          {{ content.description }}
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
