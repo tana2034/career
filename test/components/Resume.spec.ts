cf1fd5ca306ac4a011da92b6b272bc58ba19c639
tana2034 <2034tana@gmail.com>
2019-08-14 22:36:50 +0900

    #138 Hide items that have not been entered

--- test/components/Resume.spec.ts
+++ test/components/Resume.spec.ts
@@ -22,14 +22,35 @@ const localVue = createLocalVue()
 
 localVue.use(Vuex)
 
+const factory  = (details, employment, skills) => {
+  const store = new Vuex.Store({
+    modules: {
+      details: {
+        namespaced: true,
+        state: details,
+        mutations: detailsMutations
+      },
+      employment: {
+        namespaced: true,
+        state: employment,
+        mutations: employmentMutations
+      },
+      skills: {
+        namespaced: true,
+        state: skills,
+        mutations: skillsMutations
+      }
+    }
+  })
+  return shallowMount(Resume, { store, localVue })
+}
+
 describe('Resume', () => {
-  let actions
-  let store
   let wrapper
+  let state
 
   beforeEach(() => {
     const state = detailsState()
-    state.lastname = '山田'
     state.firstname = '太郎'
     state.email = 'example@example.com'
     state.birthDate = '2019-01-01'
@@ -37,74 +58,102 @@ describe('Resume', () => {
     state.tel = '000-0000-0000'
     state.summary = '設計・開発をやっています。'
     state.publicRelations = 'JavaScriptが得意です。'
-
-    actions = {
-      testAction: jest.fn()
-    }
-    store = new Vuex.Store({
-      modules: {
-        details: {
-          namespaced: true,
-          state: state,
-          actions,
-          mutations: detailsMutations
-        },
-        employment: {
-          namespaced: true,
-          state: employmentState(),
-          actions,
-          mutations: employmentMutations
-        },
-        skills: {
-          namespaced: true,
-          state: skillsState(),
-          actions,
-          mutations: skillsMutations
-        }
-      }
-    })
-    wrapper = shallowMount(Resume, { store, localVue })
   })
 
   test('is a Vue instance', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
-  test('lastname', () => {
+  test('lastname あり', () => {
+    state = detailsState()
+    state.lastname = '山田'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.lastname').html()).toContain('山田')
   })
 
-  test('firstname', () => {
+  test('firstname あり', () => {
+    state = detailsState()
+    state.firstname = '太郎'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.firstname').html()).toContain('太郎')
   })
 
-  test('email', () => {
+  test('email あり', () => {
+    state = detailsState()
+    state.email = 'example@example.com'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.email').html()).toContain('example@example.com')
   })
 
-  test('birthDate', () => {
+  test('email なし', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
+    expect(wrapper.find('.email').exists()).toBeFalsy()
+  })
+
+  test('birthDate あり', () => {
+    state = detailsState()
+    state.birthDate = '2019-01-01'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.birthDate').html()).toContain('2019-01-01')
   })
 
-  test('address', () => {
+  test('birthDate なし', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
+    expect(wrapper.find('.birthDate').exists()).toBeFalsy()
+  })
+
+  test('address あり', () => {
+    state = detailsState()
+    state.address = '東京都千代田区丸の内一丁目'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.address').html()).toContain(
       '東京都千代田区丸の内一丁目'
     )
   })
 
-  test('tel', () => {
+  test('address なし', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
+    expect(wrapper.find('.address').exists()).toBeFalsy()
+  })
+
+  test('tel あり', () => {
+    state = detailsState()
+    state.tel = '000-0000-0000'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.tel').html()).toContain('000-0000-0000')
   })
 
-  test('summary', () => {
+  test('tel なし', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
+    expect(wrapper.find('.tel').exists()).toBeFalsy()
+  })
+
+  test('summary あり', () => {
+    state = detailsState()
+    state.summary = '設計・開発をやっています。'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.summary').html()).toContain(
       '設計・開発をやっています。'
     )
   })
 
-  test('publicRelations', () => {
+  test('summary　なし', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
+    expect(wrapper.find('.summary').exists()).toBeFalsy()
+  })
+
+  test('publicRelations　あり', () => {
+    state = detailsState()
+    state.publicRelations = 'JavaScriptが得意です。'
+    wrapper = factory(state, employmentState(), skillsState())
     expect(wrapper.find('.publicRelations').html()).toContain(
       'JavaScriptが得意です。'
     )
+  })
+
+  test('publicRelations　なし', () => {
+    wrapper = factory(detailsState(), employmentState(), skillsState())
+    expect(wrapper.find('.publicRelations').exists()).toBeFalsy()
   })
 })
